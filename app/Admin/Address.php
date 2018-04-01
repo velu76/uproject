64766f479e31191de5d9f65724aba48761b0f231
Velu Ilayaraja <velu76@gmail.com>
2018-04-01 08:24:08 +0800

    added phpunit

--- app/Admin/Address.php
+++ app/Admin/Address.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    protected $fillable=['street1','street2','zipcode'];
 }
--- app/Admin/Util/Country.php
+++ app/Admin/Util/Country.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model {
 
   protected $fillable = ['code', 'name', 'top'];  
+  
 }
