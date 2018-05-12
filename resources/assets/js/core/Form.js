034e2cf8584db2a118a3180ad27d3c6e301c3952
Velu <velu76@gmail.com>
2018-05-12 20:59:44 +0800

    Removed the alert box so it does not show any empty messages

--- resources/assets/js/core/Form.js
+++ resources/assets/js/core/Form.js
@@ -112,7 +112,7 @@
      * @param {object} data
      */
     onSuccess(data) {
-        alert(data.message); // temporary
+        // alert(data.message); // temporary
 
         this.reset();
     }
