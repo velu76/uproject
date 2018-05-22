a148f364e8faf5f40d5da41862f29011152de029
Velu <velu76@gmail.com>
2018-05-22 17:10:42 +0800

    edit box for sources

--- resources/assets/js/components/admin/SourceIndex.vue
+++ resources/assets/js/components/admin/SourceIndex.vue
@@ -1,15 +1,34 @@
 <template>	
 	<div> 
-	 <table class="table">
-			<header>
+	
+	<div class="editor" v-if="edit">
+		<table>
+			<tr>
+				<td>
+					Sourcename
+				</td>
+				<td>
+					<input type="text" size=50 v-model="sourceedt" />
+				</td>
+			</tr>
+
+			<tr>
+				<td></td>
+				<td><button @click="">Change now</button> <button @click="edit=false">Close</button></td>								
+			</tr>
+		</table>
+	</div>
+
+
+	 <table>
+			<tr>
 				<th>Source</th>
 				<th>Actions</th>
-			</header>				
+			</tr>				
 			<tr v-for="source in sources.data" >	
 				<td>{{source.text}}</td>
 				<td><a class="btn btn-large" @click="editMe(source.text, source.id)">Edit</a></td>		
-			</tr>
-			
+			</tr>		
 		</table>
   	</div>
 </template>
@@ -19,13 +38,17 @@
 		props: ['tdata'],
 		data: () => {
 			return{
-				sources:''
+				sources:'',
+				edit: false,
+				sourceedt: '',
 			};
 		},	
 
 		methods: {
 			editMe(item, id){
-				alert("Editing item " + item + " with id of "  + id);
+				// alert("Editing item " + item + " with id of "  + id);
+				this.edit=true;
+				this.sourceedt=item;
 			}
 		},
 		mounted() {
@@ -34,3 +57,35 @@
         }
 	}
 </script>
+
+<style>
+table, th,td {
+	border: 1px solid black;
+}
+
+th {
+	vertical-align: middle;
+}
+
+th, td {
+	padding: 5px;
+	text-align:left;
+}
+
+.editor{
+	border: 1px solid #2b2e2e; 
+	padding: 10px 10px;
+	margin-top:10px;
+	margin-bottom:10px;
+}
+
+button {
+	padding: 3px;
+	margin:4px;
+	color:black;
+	font-size:11px;
+	background-color: hsl(171,100%,41%);
+}
+
+
+</style>
